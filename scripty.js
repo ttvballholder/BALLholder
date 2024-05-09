6e51f889bf46ed3535c13d475c878092be11db76
ttvballholder <deathawaitsimwatchingyou@gmail.com>
2024-05-09 19:04:50 +0200

    Update scripty.js

--- scripty.js
+++ scripty.js
@@ -1,5 +1,5 @@
 function downloadVideo() {
-  const videoUrl = 'videos/video.mp4'; // Change 'videos/zzart.mp4' to the relative path of your video file
+  const videoUrl = 'videos/zzart.mp4'; // Change 'videos/zzart.mp4' to the relative path of your video file
   const a = document.createElement('a');
   a.href = videoUrl;
   a.download = 'video.mp4';
