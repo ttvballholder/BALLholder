018e9a380e9b110b1a61e1fd2f6c2d1b0b0445cc
ttvballholder <deathawaitsimwatchingyou@gmail.com>
2024-05-10 22:01:36 +0200

    Delete scripty.js

--- scripty.js
+++
@@ -1,9 +0,0 @@
-function downloadVideo() {
-  const videoUrl = 'videos/zzart.mp4'; // Change 'videos/zzart.mp4' to the relative path of your video file
-  const a = document.createElement('a');
-  a.href = videoUrl;
-  a.download = 'video.mp4';
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
-}
